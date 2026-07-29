d807d69803c26b4136f73eb9f366289448128a65
amzzzt <13738504844@163.com>
2026-07-29 09:03:47 +0800

    第二题

--- project/user/src/mode_line.c
+++ project/user/src/mode_line.c
@@ -18,6 +18,7 @@ static state_t  st;
 static uint32_t follow_t0;
 static uint32_t line_t0;
 static uint32_t lap_start;
+static uint32_t lap_time;       /* 冻结的圈时 */
 
 static int is_stop_line(void) {
     return track_value(5) == 0
@@ -31,6 +32,7 @@ void mode_line_init(void)
     follow_t0 = 0;
     line_t0   = 0;
     lap_start = 0;
+    lap_time  = 0;
     tft180_show_string(0, 0, "Mode1:Line");
     tft180_show_string(0, 1, "Press KEY1");
 }
@@ -55,11 +57,19 @@ void mode_line_update(void)
 
     case S_FOLLOW: {
         int dev = track_deviation();
-        int tl = 400 + dev;
-        int tr = 400 - dev;
+        int tl = 600 + dev;
+        int tr = 600 - dev;
         if (tl >  8000) tl =  8000; if (tl < -8000) tl = -8000;
         if (tr >  8000) tr =  8000; if (tr < -8000) tr = -8000;
         motor_control_update((int16_t)tl, (int16_t)tr);
+
+        /* 实时显示时间 */
+        {
+            uint32_t t = now - lap_start;
+            char dis[16];
+            sprintf(dis, "%d.%02ds", (int)(t/1000), (int)((t%1000)/10));
+            tft180_show_string(0, 1, dis);
+        }
 
         /* 前500ms不检测 + 持续50ms消抖 */
         if (now - follow_t0 > 500) {
@@ -68,7 +78,13 @@ void mode_line_update(void)
                 if (now - line_t0 > 50) {
                     motor_stop();
                     st = S_STOP;
-                    tft180_show_string(0, 0, "STOP    ");
+                    lap_time = now - lap_start;     /* 冻结 */
+                    {
+                        char dis[16];
+                        sprintf(dis, "%d.%02ds", (int)(lap_time/1000), (int)((lap_time%1000)/10));
+                        tft180_show_string(0, 0, "STOP    ");
+                        tft180_show_string(0, 1, dis);
+                    }
                 }
             } else {
                 line_t0 = 0;
@@ -77,14 +93,9 @@ void mode_line_update(void)
         break;
     }
 
-    case S_STOP: {
+    case S_STOP:
         motor_stop();
-        uint32_t lap_ms = now - lap_start;
-        char dis[16];
-        sprintf(dis, "%d.%02ds", (int)(lap_ms/1000), (int)((lap_ms%1000)/10));
-        tft180_show_string(0, 1, dis);
         break;
-    }
     }
 
     /* === 50ms 调试打印 === */
@@ -94,7 +105,7 @@ void mode_line_update(void)
         int dev = track_deviation();
         int sl = (int)motor_control_left_speed();
         int sr = (int)motor_control_right_speed();
-        uint32_t elapsed = now - lap_start;
+        uint32_t elapsed = (st == S_STOP) ? lap_time : (now - lap_start);
         char buf[64];
         sprintf(buf, "S:%d D:%d L:%d R:%d T:%d.%02d\r\n",
                 (int)st, dev, sl, sr,
